1ad69a2d524496b244fba9751515b7dbf494e9ee
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-10-12 17:35:01 +0600

    active link work done

--- src/Components/NaveBar/NaveBar.jsx
+++ src/Components/NaveBar/NaveBar.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 const NaveBar = () => {
+
     return (
     <div>
         <div className="navbar bg-blue-100">
@@ -14,17 +15,25 @@ const NaveBar = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu md:block menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><Link to='/home'>Home</Link></li>
-                        <li><Link to='/topics'>Topics</Link></li>
-                        <li><Link to='/statistics'>Statistics</Link></li>
-                        <li><Link to='/blog'>Blog</Link></li>
+                        <li><NavLink to='/home' >Home</NavLink></li>
+                        <li><NavLink to='/topics'>Topics</NavLink></li>
+                        <li><NavLink to='/statistics'>Statistics</NavLink></li>
+                        <li><NavLink to='/blog'>Blog</NavLink></li>
                     </ul>
                 </div>
                 <ul className="md:flex hidden ">
-                    <li><Link className='mr-3 px-3 py-2 hover:text-orange-700' to='/home'>Home</Link></li>
-                    <li><Link className='mr-3 px-3 py-2 hover:text-orange-700' to='/topics'>Topics</Link></li>
-                    <li><Link className='mr-3 px-3 py-2 hover:text-orange-700' to='/statistics'>Statistics</Link></li>
-                    <li><Link className='mr-3 px-3 py-2 hover:text-orange-700' to='/blog'>Blog</Link></li>
+                    <li>
+                        <NavLink className={({isActive}) => isActive? `mr-3 px-3 py-2 text-orange-700`:`mr-3 px-3 py-2 hover:text-orange-700` } to='/home'>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={({isActive}) => isActive? `mr-3 px-3 py-2 text-orange-700`:`mr-3 px-3 py-2 hover:text-orange-700` } to='/topics'>Topics</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={({isActive}) => isActive? `mr-3 px-3 py-2 text-orange-700`:`mr-3 px-3 py-2 hover:text-orange-700` } to='/statistics'>Statistics</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={({isActive}) => isActive? `mr-3 px-3 py-2 text-orange-700`:`mr-3 px-3 py-2 hover:text-orange-700` } to='/blog'>Blog</NavLink>
+                    </li>
                 </ul>
             </div>
         </div>
