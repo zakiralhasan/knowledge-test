e67aed758b907fe9ff1033d8b7cac7d2c77c6eea
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-10-12 14:02:28 +0600

    fontawesome installed and corrent answer showing functionality done

--- src/Components/QuizPage/QuizPage.js
+++ src/Components/QuizPage/QuizPage.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
+import { toast } from 'react-toastify';
 
 const QuizPage = ({singleQuiz, index, checkCorrectAnswer}) => {
     const {question, options, correctAnswer} = singleQuiz;
+
+    const seeRightAnswer = () => {
+        toast.info(correctAnswer)
+    }
 
     const quizOptions = options.map((option, index) =>
      <button onClick={(e) => checkCorrectAnswer(e.target.innerText, correctAnswer)} className='bg-blue-200 hover:bg-blue-500 hover:text-white rounded border m-2 shadow p-2 text-center' key={index}>
@@ -11,7 +18,10 @@ const QuizPage = ({singleQuiz, index, checkCorrectAnswer}) => {
     return (
         <div> 
             <div className='bg-gray-100 border m-6'>
-                <h1 className='m-2 font-semibold'>Quiz No. {index} {question}</h1>
+                <div className='flex justify-between m-2'>
+                    <h1 className='font-semibold'>Quiz No. {index} {question}</h1>
+                    <FontAwesomeIcon onClick={seeRightAnswer} className='cursor-pointer ' icon={faEye}></FontAwesomeIcon>
+                </div>
                 <div className='grid sm:grid-cols-2 gap-2'>
                     {quizOptions}
                 </div>
